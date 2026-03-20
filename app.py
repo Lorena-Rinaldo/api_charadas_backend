e5d573126dfaf084237d8d0762a9704eadb55d67
Lorena-Rinaldo <lorena_edu@outlook.com>
2026-03-20 13:57:29 -0300

    Feat: Manutenção da rota 1 - métodos GET de todas as charadas

--- app.py
+++ app.py
@@ -1,49 +1,62 @@
 from flask import Flask, jsonify, request
 import random
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-charadas = [
-    {"pergunta": "O que é o que é? Tem cabeça, tem dente, tem barba, mas não é gente?", "resposta": "Alho"},
-    {"pergunta": "O que é o que é? Tem asa, mas não voa?", "resposta": "Galinha"},
-    {"pergunta": "O que é o que é? Tem um pescoço, mas não tem cabeça?", "resposta": "Garrafa"},
-    {"pergunta": "O que é o que é? Tem um olho, mas não pode ver?", "resposta": "Agulha"},
-    {"pergunta": "O que é o que é? Tem um coração que não bate?", "resposta": "Alcachofra"}
-]
+# Carrega as credenciais do Firebase
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
 # ----- MÉTODOS PÚBLICOS -----
 
-@app.route('/', methods=['GET'])
+# Rota PRINCIPAL - Apresentação da API
+@app.route("/", methods=["GET"])
 def root():
-    return "API TÁ ON"
+    return jsonify({
+        "api": "charadas",
+        "version" : "1.0",
+        "author" : "Lorena Rinaldo"
+    })
+
 
 # Rota 1 - Método GET - Todas as charadas
-@app.route("/charadas", methods=['GET'])
+@app.route("/charadas", methods=["GET"])
 def get_charadas():
+    charadas = [] #Lista vazia
+    lista = db.collection('charadas').stream() #stream lista todos os dados
+    
+    # Tranforma objeto do firestore em dicionário python
+    for item in lista:
+        charadas.append(item.to_dict())
     return jsonify(charadas), 200
 
+
 # Rota 2 - Método GET - Charadas aleatórias
-@app.route("/charadas/aleatorias", methods=['GET'])
+@app.route("/charadas/aleatorias", methods=["GET"])
 def get_charadas_random():
-    charada = random.choice(charadas)
+    charada = random.choice()
     return jsonify(charada), 200
+
 
 # ----- MÉTODOS PRIVADOS -----
 
 # Rota 3 - Método POST - Cadastro de novas charadas
-@app.route("/charadas", methods=['POST'])
+@app.route("/charadas", methods=["POST"])
 def post_charadas():
     dados = request.get_json()
     if not dados or "pergunta" not in dados or "resposta" not in dados:
-        return jsonify({"error":"Dados inválidos"}), 400
-    nova_charada = {
-        "pergunta": dados["pergunta"],
-        "resposta": dados["resposta"]
-    }
-    
-    charadas.append(nova_charada)
-    return jsonify({"message": "Charada criada com sucesso!", "charada": nova_charada}), 201
-        
+        return jsonify({"error": "Dados inválidos"}), 400
+    nova_charada = {"pergunta": dados["pergunta"], "resposta": dados["resposta"]}
+
+    # .append(nova_charada)
+    return (
+        jsonify({"message": "Charada criada com sucesso!", "charada": nova_charada}),
+        201,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
